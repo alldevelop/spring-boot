456bf3f3e0e4ff2ac2faa05b88f09e9f4549e647
alldevelop <remydntn@gmail.com>
2020-01-26 19:26:40 +0200

    Annotations added

--- src/main/java/com/mainacad/AppRunner.java
+++ src/main/java/com/mainacad/AppRunner.java
@@ -1,9 +1,14 @@
+
 package com.mainacad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableSwagger2
 public class AppRunner {
 
 	public static void main(String[] args) {
