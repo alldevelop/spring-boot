6523b8ee1c742c7fb5f9a05c3e28b6e80ec04fc9
alldevelop <remydntn@gmail.com>
2020-01-26 19:20:32 +0200

    Init commit with changes

---
+++ src/main/java/com/mainacad/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.mainacad.controller;
+
+import com.mainacad.model.User;
+import com.mainacad.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PutMapping
+    public ResponseEntity save(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        if (savedUser == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity update(@RequestBody User user) {
+        User savedUser = userService.update(user);
+        if (savedUser == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @PostMapping("auth")
+    public ResponseEntity getByLoginAndPassword(@RequestBody String body) {
+        Map<String, Object> map = new JacksonJsonParser().parseMap(body);
+        User user = userService.getByLoginAndPassword((String) map.get("login"), (String) map.get("password"));
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @GetMapping({"", "{id}"})
+    public ResponseEntity getUser(@PathVariable(required = false) Integer id) {
+        if (id != null) {
+            User user = userService.getById(id);
+            if (user == null) {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(userService.getAll(), HttpStatus.OK);
+        }
+    }
+}
