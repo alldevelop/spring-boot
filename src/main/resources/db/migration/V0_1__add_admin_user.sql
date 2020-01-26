e3b985e43468c9fd8a5ce5ae5186a8104d9fced8
alldevelop <remydntn@gmail.com>
2020-01-26 20:45:01 +0200

    Scheduler added

---
+++ src/main/resources/db/migration/V0_1__add_admin_user.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO
+    users(login, password, first_name, last_name)
+VALUES ('admin', '838383', 'Antony', 'Mauer');
---
+++ src/main/resources/db/migration/V1_4__add_new_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO
+    users(login, password, first_name, last_name)
+VALUES ('customer3', '123141', 'Alla', 'Koala');
